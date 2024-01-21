b782fe0d65ad5161bf32d87c80503bb506cd7223
boris.garin01 <boris.garin01job@mail.ru>
2024-01-21 22:28:58 +0300

    Bands with songs and albums relations created

---
+++ Models/Song.cs
@@ -0,0 +1,15 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Strings;
+
+namespace Metarankings.Models
+{
+    [PublishedModel("song")]
+    public class Song : PublishedContentModel
+    {
+        public Song(IPublishedContent content, IPublishedValueFallback publishedValueFallback) : base(content, publishedValueFallback)
+        {
+        }
+        public string Title => this.Value<string>("title");
+        public IHtmlEncodedString Lyrics => this.Value<IHtmlEncodedString>("lyrics");
+    }
+}
