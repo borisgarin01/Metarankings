a0c059d17329f9ccd1e499b4f920f22a6d8a2e15
boris.garin01 <boris.garin01job@mail.ru>
2024-02-18 10:29:10 +0300

    Added games reviews with styling for game details page

---
+++ Models/GameJournalistsCompanyReviewModel.cs
@@ -0,0 +1,18 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Strings;
+
+namespace Metarankings.Models
+{
+    [PublishedModel("gameJournalistsCompanyReviewModel")]
+    public class GameJournalistsCompanyReviewModel : PublishedContentModel
+    {
+        public GameJournalistsCompanyReviewModel(IPublishedContent content, IPublishedValueFallback publishedValueFallback) : base(content, publishedValueFallback)
+        {
+        }
+        public GameDetailsPageModel GameDetailsPageModel => this.Value<GameDetailsPageModel>("gameDetailsPageModel");
+        public float Score => this.Value<float>("score");
+        public string JournalistsCompanyName => this.Value<string>("journalistsCompanyName");
+        public string AuthorName => this.Value<string>("authorName");
+        public IHtmlEncodedString Text => this.Value<IHtmlEncodedString>("text");
+    }
+}
