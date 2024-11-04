96eae616b1be2909937c7dbd75f6bd9f0bcab192
Boris Garin <67055398+borisgarin01@users.noreply.github.com>
2024-11-04 19:50:33 +0300

    best-in-this-month-slideshow, new-games has been made customizatble

--- src/app/best-in-this-month-slideshow/best-in-this-month-slideshow.component.ts
+++ src/app/best-in-this-month-slideshow/best-in-this-month-slideshow.component.ts
@@ -13,31 +13,43 @@ export class BestInThisMonthSlideshowComponent {
     {
       title: 'Анора',
       link: 'https://metarankings.ru/anora-2024/',
+      score: 6.5,
+      scoreStyle: 'small-score mark-7',
       imageLink: 'https://metarankings.ru/images/uploads/2024/10/anora-2024-cover-art-187x281.jpg'
     },
     {
       title: 'Ужасающий 3',
       link: 'https://metarankings.ru/uzhasayushhij-3/',
+      score: 5.9,
+      scoreStyle: 'small-score mark-6',
       imageLink: 'https://metarankings.ru/images/uploads/2024/06/uzhasayushhij-3-cover-art-197x281.jpg'
     },
     {
       title: 'Свидание с монстром',
       link: 'https://metarankings.ru/svidanie-s-monstrom-2024/',
+      score: 6.1,
+      scoreStyle: 'small-score mark-6',
       imageLink: 'https://metarankings.ru/images/uploads/2024/10/svidanie-s-monstrom-cover-art-196x281.jpg'
     },
     {
       title: 'Вечная зима',
       link: 'https://metarankings.ru/vechnaya-zima-2024/',
+      score: 4.6,
+      scoreStyle: 'small-score mark-5',
       imageLink: 'https://metarankings.ru/images/uploads/2024/10/vechnaya-zima-cover-art-196x281.jpg'
     },
     {
       title: 'Подземная бездна',
       link: 'https://metarankings.ru/podzemnaya-bezdna-2024/',
+      score: 5.0,
+      scoreStyle: 'small-score mark-5',
       imageLink: 'https://metarankings.ru/images/uploads/2024/10/podzemnaya-bezdna-cover-art-196x281.jpg'
     },
     {
       title: 'Крик. Сезон призраков',
       link: 'https://metarankings.ru/krik-sezon-prizrakov-2024/',
+      score: 6.8,
+      scoreStyle: 'small-score mark-7',
       imageLink: 'https://metarankings.ru/images/uploads/2024/10/krik-sezon-prizrakov-2024-cover-art-190x281.jpg'
     }
   ];
--- src/app/new-movies/new-movies.component.ts
+++ src/app/new-movies/new-movies.component.ts
@@ -8,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrl: './new-movies.component.css'
 })
 export class NewMoviesComponent {
-
+  movies = [
+    {
+      href: 'https://metarankings.ru/vechnaya-zima-2024/',
+      name: 'Вечная зима',
+      imageSource: 'https://metarankings.ru/images/uploads/2024/10/vechnaya-zima-cover-art-50x70.jpg',
+      score: 6.4,
+      scoreStyle: 'small-score mark-6',
+      genres: [
+        { href: 'https://metarankings.ru/meta/movies/dramy/', name: 'Драмы' }
+      ],
+      reviews: { source: 'https://metarankings.ru/vechnaya-zima-2024/#reviews', count: 4 },
+      comments: { source: 'https://metarankings.ru/vechnaya-zima-2024/#comments', count: 0 },
+      releaseDate: new Date(2024, 10, 31),
+      description:'Фильм «Вечная зима» рассказывает о Владимире и Елене, которые по-разному реагируют на трагические события, связанные с их сыном. Елена стремится разобраться в прошлом...'
+    },
+    {
+      href: 'https://metarankings.ru/proklyatyj-ostrov-2024/',
+      name: 'Проклятый остров',
+      imageSource: 'https://metarankings.ru/images/uploads/2024/06/proklyatyj-ostrov-2024-cover-art-50x70.jpg',
+      score: 0,
+      scoreStyle: 'small-score mark-0',
+      genres: [
+        { href: 'https://metarankings.ru/meta/movies/horror/', name: 'Ужасы' }
+      ],
+      reviews: { source: 'https://metarankings.ru/proklyatyj-ostrov-2024/#reviews', count: 0 },
+      comments: { source: 'https://metarankings.ru/proklyatyj-ostrov-2024/#comments', count: 0 },
+      releaseDate: new Date(2024, 10, 31),
+      description:'В фильме «Проклятый остров» отпуск молодых друзей превращается в настоящий непрекращающийся ужас, когда они случайно будят зловещее существо на необитаемом острове, которому необходима...'
+    },
+    {
+      href: 'https://metarankings.ru/vozvrashhenie-popugaya-keshi-2024/',
+      name: 'Возвращение попугая Кеши',
+      imageSource: 'https://metarankings.ru/images/uploads/2024/10/vozvrashhenie-popugaya-keshi-cover-art-50x70.jpg',
+      score: 0,
+      scoreStyle: 'small-score mark-0',
+      genres: [
+        { href: 'https://metarankings.ru/meta/movies/comedy/', name: 'Комедии' },
+        { href: 'https://metarankings.ru/meta/movies/semejnye/', name: 'Семейные'}
+      ],
+      reviews: { source: 'https://metarankings.ru/vozvrashhenie-popugaya-keshi-2024/#reviews', count: 0 },
+      comments: { source: 'https://metarankings.ru/vozvrashhenie-popugaya-keshi-2024/#comments', count: 0 },
+      releaseDate: new Date(2024, 10, 31),
+      description:'В жизни Митяя, простого учителя географии из Сочи, появляется необычный подарок: не просто говорящий попугай, а настоящий гений из древнего рода. Иннокентий, которого...'
+    },
+  ];
 }
