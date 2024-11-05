5e71c2ab3d670a7415dd144a8004b6246d2cea5d
Boris Garin <67055398+borisgarin01@users.noreply.github.com>
2024-11-06 00:21:53 +0300

    Specials code behind set

--- src/app/specials/specials.component.ts
+++ src/app/specials/specials.component.ts
@@ -1,12 +1,34 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-specials',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './specials.component.html',
   styleUrl: './specials.component.css'
 })
 export class SpecialsComponent {
-
+  specials=[
+    {
+      href:'https://metarankings.ru/vyjdet-li-god-of-war-ragnarok-na-pk/',
+      title:'Выйдет ли God Of War Ragnarok на ПК?',
+      imageSrc:'https://metarankings.ru/images/uploads/2022/08/god-of-war-ragnarok1-445x250.jpg'
+    },
+    {
+      href:'https://metarankings.ru/stoit-li-zhdat-overwatch-2/',
+      title:'Стоит ли ждать Overwatch 2?',
+      imageSrc:'https://metarankings.ru/images/uploads/2022/08/overwatch-2-445x250.jpg'
+    },
+    {
+      href:'https://metarankings.ru/pyat-prichin-posmotret-serial-dom-drakona/',
+      title:'5 причин посмотреть сериал «Дом Дракона»',
+      imageSrc:'https://metarankings.ru/images/uploads/2022/08/dom-drakona-445x250.jpg'
+    },
+    {
+      href:'https://metarankings.ru/evolyuciya-znaka-cheloveka-pauka-za-vsyu-istoriyu-geroya-marvel/',
+      title:'Эволюция знака Человека-Паука — Как менялся логотип за всю историю героя Marvel',
+      imageSrc:'https://metarankings.ru/images/uploads/2021/10/chelovek-pauk-445x250.jpg'
+    },
+  ]
 }
