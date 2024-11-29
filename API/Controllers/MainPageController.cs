f85cf0608d78ad2b86fbc7d0e329ceb5b605998b
Boris Garin <boris.garin01job@mail.ru>
2024-11-30 00:16:14 +0300

    Первый запрос данных из Blazor в WebAPI. TODO: запросы данных из Blazor в Web API для отображения в MainPage

---
+++ API/Controllers/MainPageController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MainPageController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<IndexMoviesComponentItem>>> GetIndexMoviesComponentsItemsGenresAsync()
+    {
+        var indexMoviesComponentsItemsGenres = new IndexMoviesComponentItem[]
+        {
+            new IndexMoviesComponentItem
+            {
+                Genres=new Genre[]
+                {
+                    new Genre { Href="https://metarankings.ru/meta/movies/comedy/", Name="Комедии" },
+                    new Genre { Href="https://metarankings.ru/meta/movies/novogodnie/",Name="Новогодние" },
+                    new Genre { Href="https://metarankings.ru/meta/movies/horror/", Name="Ужасы" },
+                    new Genre { Href="https://metarankings.ru/meta/movies/fantasy-movies/", Name="Фэнтези" }
+                },
+                Title="Дорогой Санта",
+                ImageAlt="Дорогой Санта",
+                ImageSrc="https://metarankings.ru/images/uploads/2024/11/dorogoj-santa-2024-cover-art-50x70.jpg",
+                Id=1,
+                ItemHref="https://metarankings.ru/dorogoj-santa-2024/"
+            }
+        };
+        return Ok(indexMoviesComponentsItemsGenres);
+    }
+}
