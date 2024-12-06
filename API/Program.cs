3a6b8a426af80de169eca527d516fa07be47ec47
Boris Garin <boris.garin01job@mail.ru>
2024-12-06 14:50:07 +0300

    Web API был размещен на IIS

--- API/Program.cs
+++ API/Program.cs
@@ -27,12 +27,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.MapControllers();
 app.UseHttpsRedirection();
