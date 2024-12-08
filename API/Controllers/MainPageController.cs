b2d70bfd1901d7bd7e88c7f9507221c8f1c370fe
Boris Garin <boris.garin01job@mail.ru>
2024-12-08 14:28:49 +0300

    Получение платформ с играми реализовано

--- API/Controllers/MainPageController.cs
+++ API/Controllers/MainPageController.cs
@@ -1,5 +1,7 @@
-﻿using Domain;
+﻿using Data;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -7,5 +9,17 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public sealed class MainPageController : ControllerBase
 {
-    
+    private readonly DataContext dataContext;
+
+    public MainPageController(DataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<GamePlatform>> GetAll()
+    {
+        var gamesWithPlatforms = dataContext.GamesPlatforms.Include(g => g.Games).ToArray();
+        return Ok(gamesWithPlatforms);
+    }
 }
