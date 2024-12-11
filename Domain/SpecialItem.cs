f8432bafee0fda99bff7b95996b722186f655f1e
boris <boris_it@fortuna99.ru>
2024-12-11 17:09:14 +0300

    Special items have been moved to Domain project to be set from code behind (from web api - from database)

---
+++ Domain/SpecialItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+public sealed record SpecialItem
+{
+    public string Href { get; set; }
+    public string Title { get; set; }
+    public string ImageAlt { get; set; }
+    public string ImageSrc { get; set; }
+}
