40cbecfc9454df6d6ca227d076e35058364a11c1
boris <boris_it@fortuna99.ru>
2024-12-11 16:23:33 +0300

    The best in current month items have been moved to Domain project to be set from code behind (from web api - from database)

---
+++ Domain/ScoredImagedLinkedTitledItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+public sealed record ScoredImagedLinkedTitledItem
+{
+    public string Url { get; set; }
+    public string ImageSource { get; set; }
+    public float Score { get; set; }
+    public string Title { get; set; }
+}
