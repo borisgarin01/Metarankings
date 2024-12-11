bc1ae1b6e48e44c713b43d7e28987eb35b62e8a6
boris <boris_it@fortuna99.ru>
2024-12-11 16:41:24 +0300

    New games component items have been moved to Domain project to be set from code behind (from web api - from database)

---
+++ Domain/NewGame.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+public sealed record NewGame
+{
+    public string Href { get; set; }
+    public string Description { get; set; }
+    public string ImageSrc { get; set; }
+    public string ImageAlt { get; set; }
+    public string Name { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
