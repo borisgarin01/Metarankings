d80ccdca997f35c2af81d8b6bba3a9b33b21cd13
boris <boris_it@fortuna99.ru>
2024-12-11 17:19:54 +0300

    Soon at cinemas items have been moved to Domain project to be set from code behind (from web api - from database)

---
+++ Domain/SoonAtCinemasComponentItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain;
+public sealed record SoonAtCinemasComponentItem
+{
+    public string Href { get; set; }
+    public string ImageSrc { get; set; }
+    public string Name { get; set; }
+    public string OriginalName { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
