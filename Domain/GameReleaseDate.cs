3d3e7f9167eaef041ecd200642b229504d218a1d
boris <boris_it@fortuna99.ru>
2024-12-11 17:35:01 +0300

    Games releases dates have been moved to Domain project to be set from code behind (from web api - from database)

---
+++ Domain/GameReleaseDate.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+public sealed record GameReleaseDate
+{
+    public string Href { get; set; }
+    public string ImageSrc { get; set; }
+    public string Name { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
