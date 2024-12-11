ba727f6d842316b9dd3aab60accd9599173bd7ab
boris <boris_it@fortuna99.ru>
2024-12-11 16:33:05 +0300

    New movies component items have been moved to Domain project to be set from code behind (from web api - from database)

---
+++ Domain/NewMovie.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+public sealed record NewMovie
+{
+    public string Description { get; set; }
+    public string ImageHref { get; set; }
+    public string Link { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public float Score { get; set; }
+    public string Title { get; set; }
+}
