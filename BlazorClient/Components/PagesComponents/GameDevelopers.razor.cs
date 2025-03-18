057ce476f8a61cec5368e3380ff8f2142c97e395
Boris Garin <boris.garin01job@mail.ru>
2025-03-18 21:24:45 +0300

    Выделен компонент GameDevelopers

---
+++ BlazorClient/Components/PagesComponents/GameDevelopers.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorClient.Components.PagesComponents;
+
+public partial class GameDevelopers
+{
+    [Parameter]
+    public string[] Developers { get; set; }
+}
