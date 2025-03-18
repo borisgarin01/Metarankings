fe7178c135724b6af37f15fd447907a3e7f4fcfb
Boris Garin <boris.garin01job@mail.ru>
2025-03-18 21:55:45 +0300

    Выделены компоненты (GamePlatforms, GamePublisher)

---
+++ BlazorClient/Components/PagesComponents/GamePlatforms.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorClient.Components.PagesComponents;
+
+public partial class GamePlatforms
+{
+    [Parameter]
+    public string[] Platforms { get; set; }
+}
---
+++ BlazorClient/Components/PagesComponents/GamePublisher.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorClient.Components.PagesComponents;
+
+public partial class GamePublisher
+{
+    [Parameter]
+    public string Publisher { get; set; }
+}
