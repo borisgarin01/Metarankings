415711dd7ba0892bc9b22b2f4423f12cd1c331b7
Boris Garin <boris.garin01job@mail.ru>
2025-03-22 10:09:14 +0300

    Добавлены трейлеры и скриншоты в модель Game

--- Domain/Game.cs
+++ Domain/Game.cs
@@ -14,4 +14,6 @@ public sealed record Game
     public required string Localization { get; set; }
     public DateOnly? ReleaseDate { get; set; }
     public required string Description { get; set; }
+    public string Trailer { get; set; }
+    public Screenshot[] Screenshots { get; set; }
 }
---
+++ Domain/Screenshot.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain;
+
+public class Screenshot
+{
+    public string Small { get; set; }
+    public string Original { get; set; }
+}
