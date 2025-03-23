4db86dca0f2523d89af47aec820131440487f979
Boris Garin <boris.garin01job@mail.ru>
2025-03-23 21:10:38 +0300

    Выделен компонент "Жанры"

---
+++ BlazorClient/Components/PagesComponents/GenresComponent.razor.cs
@@ -0,0 +1,10 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorClient.Components.PagesComponents;
+
+public partial class GenresComponent
+{
+    [Parameter, EditorRequired]
+    public IEnumerable<Genre> Genres { get; set; }
+}
