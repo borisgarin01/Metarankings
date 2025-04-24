513c425f63e6950081e8eb111ac597349ec916e6
Boris Garin <boris.garin01job@mail.ru>
2025-04-24 13:29:55 +0300

    Создан DTO Developer

---
+++ Domain/Developer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Domain;
+
+[Table("Developers")]
+public sealed record Developer
+{
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+}
