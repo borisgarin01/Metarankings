7d73c90b888b6dd80c526e314ba43c9c8c74c132
Boris Garin <boris.garin01job@mail.ru>
2025-04-24 13:26:02 +0300

    Миграция - создание таблицы Platforms

---
+++ Data/Migrations/AddPlatformsTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace Data.Migrations;
+
+[Migration(4, "Add platforms table migration")]
+public sealed class AddPlatformsTable : Migration
+{
+    public override void Down()
+    {
+        Execute.Sql("DROP TABLE Platforms;");
+    }
+
+    public override void Up()
+    {
+        Execute.Sql(@"CREATE TABLE Platforms 
+(Id bigserial not null primary key,
+Href varchar(511) not null unique,
+Name varchar(127) not null unique);");
+    }
+}
