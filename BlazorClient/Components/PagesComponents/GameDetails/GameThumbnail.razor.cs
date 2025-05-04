b832b21d5db1d25b88c0ffdfbc791af3597541b0
Boris Garin <boris.garin01job@mail.ru>
2025-05-04 23:27:06 +0300

    GameThumbnail component separation

---
+++ BlazorClient/Components/PagesComponents/GameDetails/GameThumbnail.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorClient.Components.PagesComponents.GameDetails;
+
+public partial class GameThumbnail : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public string Image { get; set; }
+
+    [Parameter, EditorRequired]
+    public string Name { get; set; }
+
+    [Parameter, EditorRequired]
+    public int? ReleaseYear { get; set; }
+}
