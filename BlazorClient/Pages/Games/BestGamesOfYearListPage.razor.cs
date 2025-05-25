4cb062815970dbdf57a90dcf10595717f88b58d2
Boris Garin <boris.garin01job@mail.ru>
2025-05-25 21:10:32 +0300

    Комбинированная фильтрация по годам выхода, разработчикам, жанрам, издателям и платформам добавлена.

--- BlazorClient/Pages/Games/BestGamesOfYearListPage.razor.cs
+++ BlazorClient/Pages/Games/BestGamesOfYearListPage.razor.cs
@@ -1,17 +1,27 @@
 ﻿using Domain;
 using Microsoft.AspNetCore.Components;
-using System;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace BlazorClient.Pages.Games;
 
 public partial class BestGamesOfYearListPage : ComponentBase
 {
-    [Parameter]
+    [SupplyParameterFromQuery]
     public int? Year { get; set; }
 
     [SupplyParameterFromQuery]
     public int? GenreId { get; set; }
+
+    [SupplyParameterFromQuery]
+    public int? PlatformId { get; set; }
+
+    [SupplyParameterFromQuery]
+    public int? DeveloperId { get; set; }
+
+    [SupplyParameterFromQuery]
+    public int? PublisherId { get; set; }
+
 
     public IEnumerable<GameModel> Games { get; set; }
 
@@ -20,16 +30,29 @@ public partial class BestGamesOfYearListPage : ComponentBase
 
     protected override async Task OnParametersSetAsync()
     {
-        if (!Year.HasValue)
+        var gamesGettingRequestModel = new GamesGettingRequestModel
         {
-            if (!GenreId.HasValue)
-                Games = await HttpClient.GetFromJsonAsync<IEnumerable<GameModel>>($"/api/Games/5/1");
-            else
-                Games = await HttpClient.GetFromJsonAsync<IEnumerable<GameModel>>($"/api/Games/5/1");
-        }
-        else
+            DevelopersIds = DeveloperId.HasValue ? [DeveloperId.Value] : null,
+            GenresIds = GenreId.HasValue ? [GenreId.Value] : null,
+            PlatformsIds = PlatformId.HasValue ? [PlatformId.Value] : null,
+            ReleasesYears = Year.HasValue ? [Year.Value] : null,
+            PublishersIds = PublisherId.HasValue ? [PublisherId.Value] : null
+        };
+
+        var httpResponseMessage = await HttpClient.PostAsJsonAsync<GamesGettingRequestModel>($"{HttpClient.BaseAddress}api/games/gamesByParameters", gamesGettingRequestModel);
+
+        if (httpResponseMessage.IsSuccessStatusCode)
         {
-            Games = await HttpClient.GetFromJsonAsync<IEnumerable<GameModel>>($"/api/Games/Year/{Year.Value}");
+            try
+            {
+                var games = await JsonSerializer.DeserializeAsync<IEnumerable<GameModel>>(await httpResponseMessage.Content.ReadAsStreamAsync());
+
+                Games = games;
+            }
+            catch
+            {
+                Games = Enumerable.Empty<GameModel>();
+            }
         }
     }
 }
