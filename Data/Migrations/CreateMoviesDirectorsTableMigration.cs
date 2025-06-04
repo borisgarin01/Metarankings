cd42254610beaa0bc613920052bb2ecda235a9d8
Boris Garin <boris.garin01job@mail.ru>
2025-06-04 22:13:19 +0300

    Movies studios and directors table migrations have been added

---
+++ Data/Migrations/CreateMoviesDirectorsTableMigration.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Data.Migrations;
+
+[Migration(19, "Create movies directors table migration")]
+public sealed class CreateMoviesDirectorsTableMigration : Migration
+{
+    public override void Down()
+    {
+        Execute.Sql("DROP TABLE MoviesDirectors");
+    }
+
+    public override void Up()
+    {
+        Execute.Sql(@"CREATE TABLE MoviesDirectors (id bigserial not null primary key,
+name varchar(511) not null unique,
+href varchar(1023) not null unique);");
+    }
+}
---
+++ Data/Migrations/CreateMoviesStudiosTableMigration.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Data.Migrations;
+
+[Migration(18, "Create movies genres table migration")]
+public sealed class CreateMoviesStudiosTableMigration : Migration
+{
+    public override void Down()
+    {
+        Execute.Sql("DROP TABLE MoviesStudios;");
+    }
+
+    public override void Up()
+    {
+        Execute.Sql(@"CREATE TABLE MoviesStudios (id bigserial not null primary key,
+name varchar(511) not null unique,
+href varchar(1023) not null unique);");
+    }
+}
