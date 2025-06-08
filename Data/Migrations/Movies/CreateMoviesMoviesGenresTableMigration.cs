b0f59fe145bcff0d8b28caaa33d4f3469f86814f
Boris Garin <boris.garin01job@mail.ru>
2025-06-08 09:33:45 +0300

    Creation of movies and movies-with-movies-genres relation tables have been added

---
+++ Data/Migrations/Movies/CreateMoviesMoviesGenresTableMigration.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace Data.Migrations.Movies;
+
+[Migration(21, "Create movies movies genres table migration")]
+public sealed class CreateMoviesMoviesGenresTableMigration : Migration
+{
+    public override void Down()
+    {
+        Execute.Sql("DROP TABLE MoviesMoviesGenres;");
+    }
+
+    public override void Up()
+    {
+        Execute.Sql(@"CREATE TABLE MoviesMoviesGenres
+(Id bigserial not null primary key,
+MovieId bigint not null,
+MovieGenreId bigint not null,
+FOREIGN KEY (MovieId) 
+REFERENCES Movies(Id) 
+ON DELETE CASCADE 
+ON UPDATE CASCADE,
+FOREIGN KEY (MovieGenreId) 
+REFERENCES MoviesGenres(Id)
+ON DELETE CASCADE
+ON UPDATE CASCADE);");
+    }
+}
---
+++ Data/Migrations/Movies/CreateMoviesTableMigration.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace Data.Migrations.Movies;
+
+[Migration(20, "Create movies table migration")]
+public sealed class CreateMoviesTableMigration : Migration
+{
+    public override void Down()
+    {
+        Execute.Sql("DROP TABLE Movies;");
+    }
+
+    public override void Up()
+    {
+        Execute.Sql(@"CREATE TABLE Movies 
+(Id bigserial not null primary key,
+Name varchar(255) not null unique,
+ImageSource varchar(1023) null unique,
+OriginalName varchar(255) not null unique,
+PremierDate date not null,
+Description varchar(1023) null);");
+    }
+}
