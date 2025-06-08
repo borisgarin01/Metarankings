75c4b75b33df7ab1bc9c99a65707378155dae6a3
Boris Garin <boris.garin01job@mail.ru>
2025-06-08 09:56:15 +0300

    Genres inserting migraiton has been added

---
+++ Data/Migrations/Movies/InsertData/InsertGenresMigration.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator;
+
+namespace Data.Migrations.Movies.InsertData;
+
+[Migration(22, "Insert genres migration")]
+public sealed class InsertGenresMigration : Migration
+{
+    public override void Down()
+    {
+        Execute.Sql(@"DELETE FROM MoviesGenres
+WHERE Name in ('Боевик', 'Военный', 'Комедия', 'Ужас', 'Фантастика',
+'Триллер', 'Фэнтези', 'Исторический', 'Мелодрама', 'Драма', 
+'Семейный', 'Новогодний', 'Мультфильм', 'Сериал');");
+    }
+
+    public override void Up()
+    {
+        Execute.Sql(@"INSERT INTO MoviesGenres (Name, Href) VALUES
+('Боевик', '/moviesGenres/action-movies'),
+('Военный', '/moviesGenres/war'),
+('Комедия', '/moviesGenres/comedies'),
+('Ужас', '/moviesGenres/horror'),
+('Фантастика', '/moviesGenres/fiction'),
+('Триллер', '/moviesGenres/thriller'),
+('Фэнтези', '/moviesGenres/fantasy'),
+('Исторический', '/moviesGenres/historical'),
+('Мелодрама', '/moviesGenres/melodrama'),
+('Драма', '/moviesGenres/dramas'),
+('Семейный', '/moviesGenres/family'),
+('Новогодний', '/moviesGenres/new-year'),
+('Мультфильм', '/moviesGenres/cartoon'),
+('Сериал', '/moviesGenres/series');");
+    }
+}
