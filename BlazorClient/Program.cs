a61cee704a848916f8aedf2d0efa41d223b3f60c
boris.garin01job <boris.garin01job@mail.ru>
2025-07-08 22:45:01 +0300

    FIX: backend url changed

--- BlazorClient/Program.cs
+++ BlazorClient/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://192.168.1.102:5001") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://192.168.1.101:5001") });
 
 await builder.Build().RunAsync();
