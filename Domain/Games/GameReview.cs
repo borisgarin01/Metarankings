cc4a5f979c7789d6e5e438b8810a03def75c9f6f
Boris Garin <boris.garin01@mail.ru>
2025-07-17 13:31:43 +0300

    Создана сущность "Комментарий"

---
+++ Domain/Games/GameReview.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Games;
+
+public sealed record GameReview
+{
+    public long Id { get; set; }
+    public long GameId { get; set; }
+    public long ApplicationUserId { get; set; }
+    public string ReviewText { get; set; }
+}
