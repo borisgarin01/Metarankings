c8c12cd95ad355f80f9b06dbe16a87e96671a7f6
boris.garin01job <boris.garin01job@mail.ru>
2025-07-22 07:10:23 +0300

    Реализован выход из учётной записи

---
+++ BlazorClient/Pages/Games/Auth/Logout.razor.cs
@@ -0,0 +1,30 @@
+﻿
+using BlazorClient.Auth;
+using IdentityLibrary.Models;
+
+namespace BlazorClient.Pages.Games.Auth;
+
+public partial class Logout : ComponentBase
+{
+    [Inject]
+    public IAuthService AuthService { get; set; }
+
+    [Inject]
+    public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            await AuthService.LogoutAsync();
+            NavigationManager.NavigateTo("/");
+        }
+        catch (Exception ex)
+        {
+             //Log exception
+        }
+    }
+}
