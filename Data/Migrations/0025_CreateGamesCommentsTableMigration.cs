14d5c5e20c90ad4dd13974c66c3148ae86d437d8
Boris Garin <boris.garin01@mail.ru>
2025-07-30 13:18:03 +0300

    Добавлена миграция создания таблицы с комментариями

---
+++ Data/Migrations/0025_CreateGamesCommentsTableMigration.cs
@@ -0,0 +1,25 @@
+﻿namespace Data.Migrations;
+
+[Migration(26, "Create games comments table")]
+public sealed class CreateGamesCommentsTableMigration : Migration
+{
+    public override void Down()
+    {
+        Execute.Sql("DROP TABLE GamesComments;");
+    }
+
+    public override void Up()
+    {
+        Execute.Sql(@"
+CREATE TABLE GamesComments
+(GameId bigint not null,
+UserId bigint not null,
+TextContent text not null,
+FOREIGN KEY(GameId) 
+    REFERENCES Games(Id)
+    ON DELETE CASCADE,
+FOREIGN KEY(UserId)
+    REFERENCES ApplicationUsers(Id)
+    ON DELETE CASCADE);");
+    }
+}
