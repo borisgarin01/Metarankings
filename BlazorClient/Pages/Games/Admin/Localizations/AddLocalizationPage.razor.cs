c34daa3ca331eabb3229fcea70107706fce5b99b
boris.garin01job <boris.garin01job@mail.ru>
2025-08-23 22:40:25 +0300

    ADD: Localizations adding frontend

---
+++ BlazorClient/Pages/Games/Admin/Localizations/AddLocalizationPage.razor.cs
@@ -0,0 +1,33 @@
+﻿using Domain.RequestsModels.Games.Localizations;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BlazorClient.Pages.Games.Admin.Localizations;
+
+[Authorize(Policy = "Admin")]
+public partial class AddLocalizationPage : ComponentBase
+{
+    [Inject]
+    public HttpClient HttpClient { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    public AddLocalizationModel AddLocalizationModel { get; } = new AddLocalizationModel();
+    protected override Task OnInitializedAsync()
+    {
+        return base.OnInitializedAsync();
+    }
+
+    public async Task AddLocalizationAsync()
+    {
+        HttpResponseMessage httpResponseMessage = await HttpClient.PostAsJsonAsync("/api/Localizations", AddLocalizationModel);
+        if (httpResponseMessage is not null && httpResponseMessage.IsSuccessStatusCode)
+            NavigationManager.NavigateTo("/admin/localizations/list-localizations");
+        else
+            if (httpResponseMessage is not null)
+            await JSRuntime.InvokeVoidAsync("alert", await httpResponseMessage.Content.ReadAsStringAsync());
+    }
+}
