3d159cac53ecaf3269c2ad9f3da35967bda860e4
boris.garin01job <boris.garin01job@mail.ru>
2025-08-24 15:14:02 +0300

    ADD: добавлен проект с web clients

---
+++ WebClientServices/IWebManager.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Games;
+using Microsoft.AspNetCore.Http;
+
+namespace WebManagers;
+
+public interface IWebManager<T, TAdd, TUpdate> where T : class, new()
+{
+    Task<IEnumerable<Developer>> GetAllAsync();
+    Task<IEnumerable<Developer>> GetAllAsync(long offset, long limit);
+    Task<IEnumerable<Developer>> GetAsync(long id);
+    Task<IEnumerable<Developer>> DeleteAsync(long id);
+    Task<IEnumerable<Developer>> UpdateAsync(long id, TUpdate tUpdate);
+    Task<HttpResponseMessage> AddFromJsonAsync(IEnumerable<TAdd> adds);
+    Task<HttpResponseMessage> AddFromExcelAsync(IFormFile formFile);
+    Task<HttpResponseMessage> AddAsync(TAdd tAdd);
+}
