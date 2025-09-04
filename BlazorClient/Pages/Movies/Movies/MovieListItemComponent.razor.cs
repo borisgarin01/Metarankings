d6cdaec3b26f723545cd7b62296c648c6d6db5df
Boris Garin <boris.garin01@mail.ru>
2025-09-04 10:41:59 +0300

    ADD: добавлены компоненты списков игр и фильмов

---
+++ BlazorClient/Pages/Movies/Movies/MovieListItemComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Movies;
+
+namespace BlazorClient.Pages.Movies.Movies;
+
+public partial class MovieListItemComponent : ComponentBase
+{
+    [Inject]
+    public HttpClient HttpClient { get; set; }
+
+    [Parameter, EditorRequired]
+    public Movie Movie { get; set; }
+}
