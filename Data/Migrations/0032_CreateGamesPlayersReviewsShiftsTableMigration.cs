590b43f22d561eacee9eefd5b17ccea246da688b
Boris Garin <boris.garin01@mail.ru>
2025-09-04 17:40:51 +0300

    ADD: tables for ratings shifts has been created (1 - up, 0 - down)

---
+++ Data/Migrations/0032_CreateGamesPlayersReviewsShiftsTableMigration.cs
@@ -0,0 +1,26 @@
+﻿namespace Data.Migrations;
+
+[Migration(32, "Create games players reviews shifts table")]
+public sealed class CreateGamesPlayersReviewsShiftsTableMigration : Migration
+{
+    public override void Down()
+    {
+        Execute.Sql("DROP TABLE GamesPlayersReviewsShifts;");
+    }
+
+    public override void Up()
+    {
+        Execute.Sql(@"CREATE TABLE GamesPlayersReviewsShifts
+(Id bigint not null primary key identity(1,1), 
+GamePlayerReviewId bigint not null,
+ShifterId bigint not null,
+Direction bit not null default 1, -- 1 - up, 0 - down
+UNIQUE(GamePlayerReviewId, ShifterId),
+FOREIGN KEY (GamePlayerReviewId) 
+REFERENCES GamesPlayersReviews(Id)
+ON DELETE CASCADE,
+FOREIGN KEY (ShifterId)
+REFERENCES ApplicationUsers(Id)
+ON DELETE NO ACTION);");
+    }
+}
---
+++ Data/Migrations/0033_CreateViewersMoviesReviewsShiftsTableMigration.cs
@@ -0,0 +1,26 @@
+﻿namespace Data.Migrations;
+
+[Migration(33, "Create viewers movies reviews shifts table")]
+public sealed class CreateViewersMoviesReviewsShiftsTableMigration : Migration
+{
+    public override void Down()
+    {
+        Execute.Sql("DROP TABLE ViewersMoviesReviewsShifts;");
+    }
+
+    public override void Up()
+    {
+        Execute.Sql(@"CREATE TABLE ViewersMoviesReviewsShifts
+(Id bigint not null primary key identity(1,1), 
+ViewerMovieReviewId bigint not null,
+ShifterId bigint not null,
+Direction bit not null default 1, -- 1 - up, 0 - down
+UNIQUE(ViewerMovieReviewId, ShifterId),
+FOREIGN KEY (ViewerMovieReviewId) 
+REFERENCES ViewersMoviesReviews(Id)
+ON DELETE CASCADE,
+FOREIGN KEY (ShifterId)
+REFERENCES ApplicationUsers(Id)
+ON DELETE NO ACTION);");
+    }
+}
