186222c69d2590498c9988a0a8ba8a206ba86ab6
@boris <boris_it@fortuna99.ru>
2026-05-22 16:52:22 +0300

    ADD: добавлены страницы Новые фильмы и Скоро выходят

---
+++ BlazorClient/Pages/Movies/Movies/ComingSoon.razor.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Movies;
+using WebManagers.Derived.Movies;
+
+namespace BlazorClient.Pages.Movies.Movies;
+
+public partial class ComingSoon : ComponentBase
+{
+    private IEnumerable<Movie> movies;
+
+    [Inject]
+    public MoviesWebManager MoviesWebManager { get; set; }
+
+    public IEnumerable<Movie> Movies
+    {
+        get => movies;
+        private set
+        {
+            movies = value;
+            StateHasChanged();
+        }
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Movies = await MoviesWebManager.GetAllAsync();
+    }
+}
---
+++ BlazorClient/Pages/Movies/Movies/NewMovies.razor.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Movies;
+using WebManagers.Derived.Movies;
+
+namespace BlazorClient.Pages.Movies.Movies;
+
+public sealed partial class NewMovies : ComponentBase
+{
+    private IEnumerable<Movie> movies;
+
+    public IEnumerable<Movie> Movies
+    {
+        get => movies;
+        private set
+        {
+            movies = value;
+            StateHasChanged();
+        }
+    }
+
+    [Inject]
+    public MoviesWebManager MoviesWebManager { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Movies = await MoviesWebManager.GetAllAsync();
+    }
+}
