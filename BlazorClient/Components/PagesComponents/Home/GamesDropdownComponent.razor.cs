de482e93270aab377aaf8d5ea68b7733cb79cd5d
boris.garin01job <boris.garin01job@mail.ru>
2026-08-10 11:51:34 +0300

    ADD: games and movies dropdown menu items

---
+++ BlazorClient/Components/PagesComponents/Home/GamesDropdownComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Games;
+
+namespace BlazorClient.Components.PagesComponents.Home;
+
+public partial class GamesDropdownComponent : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public IEnumerable<Platform> Platforms { get; set; }
+
+    [Parameter, EditorRequired]
+    public IEnumerable<Genre> Genres { get; set; }
+
+    private IEnumerable<int> GetYears()
+    {
+        var currentYear = DateTime.Now.Year;
+        return Enumerable.Range(currentYear - 8, 9).Reverse().ToList();
+    }
+}
---
+++ BlazorClient/Components/PagesComponents/Home/MoviesDropdownComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Movies;
+
+namespace BlazorClient.Components.PagesComponents.Home;
+
+public partial class MoviesDropdownComponent : ComponentBase
+{
+    [Parameter] public IEnumerable<MovieGenre> Genres { get; set; }
+
+    private List<int> GetYears()
+    {
+        var currentYear = DateTime.Now.Year;
+        return Enumerable.Range(currentYear - 8, 9).Reverse().ToList();
+    }
+}
