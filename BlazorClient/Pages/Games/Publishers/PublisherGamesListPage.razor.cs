b4169522a7ffed9efe8a6b9c86b991e00fb05b23
boris.garin01job <boris.garin01job@mail.ru>
2026-08-11 17:58:26 +0300

    UPD: developers links

--- BlazorClient/Pages/Games/Publishers/PublisherGamesListPage.razor.cs
+++ BlazorClient/Pages/Games/Publishers/PublisherGamesListPage.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Games;
+using Domain.RequestsModels.Games.Developers;
 using Domain.RequestsModels.Games.Platforms;
 using Domain.RequestsModels.Games.Publishers;
 using WebManagers;
@@ -16,18 +17,24 @@ public partial class PublisherGamesListPage : ComponentBase
     [Inject]
     public IWebManager<Publisher, AddPublisherModel, UpdatePublisherModel> PublishersWebManager { get; set; }
 
+    [Inject]
+    public IWebManager<Developer, AddDeveloperModel, UpdateDeveloperModel> DevelopersWebManager { get; set; }
+
     public Publisher Publisher { get; set; }
     public IEnumerable<Platform> Platforms { get; set; }
+    public IEnumerable<Developer> Developers { get; set; }
 
     protected override async Task OnParametersSetAsync()
     {
         Task<Publisher> publisherGettingTask = PublishersWebManager.GetAsync(PublisherId);
         Task<IEnumerable<Platform>> platformsGettingTask = PlatformsWebManager.GetFirstAsync(0, 5);
+        Task<IEnumerable<Developer>> developersGettingTask = DevelopersWebManager.GetFirstAsync(0, 5);
 
         await Task.WhenAll(publisherGettingTask, platformsGettingTask).ContinueWith(b =>
         {
             Publisher = publisherGettingTask.Result;
             Platforms = platformsGettingTask.Result;
+            Developers = developersGettingTask.Result;
         });
     }
 }
