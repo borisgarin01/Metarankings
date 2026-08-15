5d4c01357259fa24c949dd9da5da05f6f953a683
boris.garin01job <boris.garin01job@mail.ru>
2026-08-15 11:02:41 +0300

    ADD: showing, hiding background

--- BlazorClient/Components/PagesComponents/Home/GamesDropdownComponent.razor.cs
+++ BlazorClient/Components/PagesComponents/Home/GamesDropdownComponent.razor.cs
@@ -4,6 +4,11 @@ namespace BlazorClient.Components.PagesComponents.Home;
 
 public partial class GamesDropdownComponent : ComponentBase
 {
+    private bool isGamesOpen;
+    private string gamesDropdownClass => isGamesOpen ? "show" : "";
+
+    private void ShowGames() => isGamesOpen = true;
+    private void HideGames() => isGamesOpen = false;
 
     [Parameter, EditorRequired]
     public IEnumerable<Platform> Platforms { get; set; }
--- BlazorClient/Components/PagesComponents/Home/MoviesDropdownComponent.razor.cs
+++ BlazorClient/Components/PagesComponents/Home/MoviesDropdownComponent.razor.cs
@@ -4,6 +4,16 @@ namespace BlazorClient.Components.PagesComponents.Home;
 
 public partial class MoviesDropdownComponent : ComponentBase
 {
+    // State tracks if dropdowns are active
+    private bool isMoviesOpen;
+
+    // Computed CSS classes based on C# state
+    private string moviesDropdownClass => isMoviesOpen ? "show" : "";
+
+    // Handlers to change state instantly inside WebAssembly
+    private void ShowMovies() => isMoviesOpen = true;
+    private void HideMovies() => isMoviesOpen = false;
+
     [Parameter, EditorRequired]
     public IEnumerable<MovieGenre> Genres { get; set; }
 }
