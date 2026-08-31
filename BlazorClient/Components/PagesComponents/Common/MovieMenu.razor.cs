50644346a8664ef63293a8060dda99dbc98a250c
boris <boris_it@fortuna99.ru>
2026-08-31 13:02:21 +0300

    UPD: MovieMenu

---
+++ BlazorClient/Components/PagesComponents/Common/MovieMenu.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorClient.Components.PagesComponents.Common;
+
+public partial class MovieMenu : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public int? Year { get; set; }
+}
